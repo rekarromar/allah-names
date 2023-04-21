0f87510c41bd9473421679e59770b80f8a900e8b
Rekar Omar <rekarromar@gmail.com>
2023-04-21 19:15:05 +0300

    read me file

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
-<template>
-  <div class="p-4 grid gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-    <div v-for="(name, index) in data" :key="name">
-      <NuxtLink :to="`${index + 1}`">
-        <NamesContainer :name="name" :number="index + 1" />
-      </NuxtLink>
-    </div>
-  </div>
-</template>
-
 <script setup>
 import names from "../assets/database/names.json";
 const data = names;
 </script>
+
+<template>
+  <div class="flex flex-col items-center">
+    <div class="p-4 grid gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div v-for="(name, index) in data" :key="name">
+        <NuxtLink :to="`${index + 1}`">
+          <NamesContainer :name="name" :number="index + 1" />
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
