d23b28ccc44242cfdc6f5137e7855ac38526f973
Rekar Omar <rekarromar@gmail.com>
2023-05-24 09:32:00 +0300

    vue analytics

--- components/Header.vue
+++ components/Header.vue
@@ -2,11 +2,13 @@
 
 <template>
   <div class="w-full flex flex-col items-center">
-    <h1
+    <a
+      href="/"
       class="font-quran text-black text-5xl md:text-6xl text-center mb-7 uppercase"
     >
       وَلِلّهِ الأَسْمَاء الْحُسْنَى
-    </h1>
+    </a>
+
     <h3 class="font-name text-sm text-center">
       ... - 中文 - 日本語 - Русский - کوردی - Español - Turkish
     </h3>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["nuxt-icon"],
+  plugins: [{ src: '~/plugins/vercel.js', mode: 'client' }],
   app: {
     // pageTransition: { name: "page", mode: "default",  },
   },
   css: ["~/assets/css/main.css"],
+  
   postcss: {
     plugins: {
       tailwindcss: {},
